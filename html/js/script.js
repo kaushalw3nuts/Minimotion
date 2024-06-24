8413ed602362d9153895e61733619229c833f680
kaushal <kaushal.w3nuts@gmail.com>
2024-06-24 11:47:26 +0530

    homepage mobile responsive start

--- html/js/script.js
+++ html/js/script.js
@@ -1,6 +1,17 @@
 
 $(document).ready(function() {
 
+	// ====== mobile menu ======
+
+	$(".main_header .toggle-btn").click(function(e){
+		$(this).toggleClass("active");
+		$(".main_header").toggleClass("slide");
+		$("body").toggleClass("open-nav");
+		e.preventDefault();
+	});
+
+	// ====== mobile menu ======
+
 	// ===== Niceselect: Start =====
 
 	$('select').niceSelect();
@@ -11,13 +22,31 @@ $(document).ready(function() {
 
 	let productCarousel = new Swiper('.review_slider', {
 		direction: 'horizontal',
-		slidesPerView: 2,
-		// loop: true,
 		spaceBetween: 30,
 		speed: 1000,
 		navigation: {
 			nextEl: '.swiper-btn-next',
 			prevEl: '.swiper-btn-prev'
+		},
+		breakpoints: {
+			767: {
+				slidesPerView: 1,
+			},
+			991: {
+			  slidesPerView: 2,
+			},
+			1199: {
+			  slidesPerView: 2,
+			},
+			1300: {
+			  slidesPerView: 2,
+			},
+			1399: {
+				slidesPerView: 2,
+			},
+			1599: {
+				slidesPerView: 2,
+			}
 		},
 	});
 
